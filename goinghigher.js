cee79936e5470fcc2fd17b6328a357c67f869a61
NewtonOmbese <newtonombese1@gmail.com>
2022-05-26 22:17:02 +0300

    strings and numbers

--- goinghigher.js
+++ goinghigher.js
@@ -29,7 +29,30 @@ lapsIncreased()
 lapsIncreased()
 console.log(lapsCompleted)
 
+let username = "Newton"
+let message = "You have three new notifications";
 
-let message = "You have tree new notifications";
+let messageToUser = message + ', ' + username + '!' 
 
-console.log(message)
+console.log(messageToUser)
+
+let name = "Bofulo"
+let greeting = "Hi, my name is "
+
+let myGreeting = greeting + name;
+
+console.log(myGreeting)
+
+let points = 4
+let bonusPts = "10"
+
+let totalPoints = points + bonusPts;
+
+console.log(totalPoints)
+
+// strings vs numbers
+console.log(4 + 5) //9
+console.log("2" + "4")  //24
+console.log("5" + 1)  //51
+console.log(100 + "100") //100100
+
